185aaf021f64b1bc31ae556643cd11048d63d021
HaySsor <hayss007@gmail.com>
2022-05-06 15:57:58 +0200

    1 file modified

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const listBody = document.querySelector('.list-body');
 const previus = document.querySelector("[data-value='0']")
 const next = document.querySelector("[data-value='4']")
 const nameArray = [];
-
 let j = 1
+
+
 names.forEach((name, index) => {
   nameArray.push(name.textContent);
 });
@@ -25,41 +26,47 @@ function creatElement(idex1, index2) {
           </div> 
           `;
 }
-function btn(index) {
+function paginationBtn(index) {
 
   switch (index) {
     case 0:
+      return (j-1)===0? paginationBtn(1) : paginationBtn(j-1)  
 
-      return (j-1)===0? btn(1) : btn(j-1)  
     case 1:
       previus.style.pointerEvents= "none"
       previus.classList.add('disable')
       next.style.pointerEvents= "auto"
       next.classList.remove('disable')
       creatElement(nameArray[0], nameArray[1]);
+
       break;
+
     case 2:
       previus.style.pointerEvents= "auto"
       previus.classList.remove('disable')
       next.style.pointerEvents= "auto"
       next.classList.remove('disable')
       creatElement(nameArray[2], nameArray[3]);
+
       break;
+
     case 3:
       previus.style.pointerEvents= "auto"
       previus.classList.remove('disable')
       next.style.pointerEvents= "none"
       next.classList.add('disable')
       creatElement(nameArray[4], nameArray[5]);
+
       break;
     case 4:
-      return (j+1)===4? btn(3): btn(j+1) 
+      return (j+1)===4? paginationBtn(3): paginationBtn(j+1) 
+
   }
   j = index;
 }
 pages.forEach((page, index) => {
   page.addEventListener('click', () => {
-    btn(index);
+    paginationBtn(index);
   });
 });
 

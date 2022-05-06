3f6cc61a009a7a0dc54d4cc71ebbc7f64cfbd725
HaySsor <hayss007@gmail.com>
2022-05-06 15:53:29 +0200

    2 files modified

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import './style.css';
 const names = document.querySelectorAll('[data-name]');
 const pages = document.querySelectorAll('.page-link');
 const listBody = document.querySelector('.list-body');
+const previus = document.querySelector("[data-value='0']")
+const next = document.querySelector("[data-value='4']")
 const nameArray = [];
 
 let j = 1
@@ -27,14 +29,27 @@ function btn(index) {
 
   switch (index) {
     case 0:
-      return (j-1)===0? btn(1): btn(j-1)  
+
+      return (j-1)===0? btn(1) : btn(j-1)  
     case 1:
+      previus.style.pointerEvents= "none"
+      previus.classList.add('disable')
+      next.style.pointerEvents= "auto"
+      next.classList.remove('disable')
       creatElement(nameArray[0], nameArray[1]);
       break;
     case 2:
+      previus.style.pointerEvents= "auto"
+      previus.classList.remove('disable')
+      next.style.pointerEvents= "auto"
+      next.classList.remove('disable')
       creatElement(nameArray[2], nameArray[3]);
       break;
     case 3:
+      previus.style.pointerEvents= "auto"
+      previus.classList.remove('disable')
+      next.style.pointerEvents= "none"
+      next.classList.add('disable')
       creatElement(nameArray[4], nameArray[5]);
       break;
     case 4:
